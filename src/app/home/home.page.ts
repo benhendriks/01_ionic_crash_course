465aab00b59e9c19629c8187639fc57b1fe1577f
Benjamin Steidl <localartpool@gmail.com>
2021-01-11 14:52:23 +0100

    Installed ng cirle progress

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  text = 'Default starting text';
+
+  onChangeText() {
+    this.text = 'Changed!';
+  }
+
   constructor() {}
 
 }
